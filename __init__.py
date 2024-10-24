702c22ea55a442e866563bbeb13e2ce86e367773
Rasmus Jensen <rasmuskjeldsenj@gmail.com>
2024-10-24 16:12:30 +0200

    Added setup.py and renamed src to sbatch

---
+++ __init__.py
@@ -0,0 +1,1 @@
+import sbatch as sbatch
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="sbatch",
+    version="0.1.0",
+    packages=["sbatch"],
+    include_package_data=True,
+    author_email = 'rasmuskjeldsenj@gmail.com',
+    install_requires=[],
+)
